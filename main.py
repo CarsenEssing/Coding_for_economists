dd828c1f54c292d8bc408c5a6386c73f1e7cc46b
carsen essing <carsenessing1@gmail.com>
2024-01-16 13:59:53 +0000

    coded solution to string slicing excercise

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #1 Naviagte back 
+ #excercise: indexing and slicing
+
+url = "https://github.com/codedthinking/tender-home-bias/releases/download/v1.0/ted-sample.csv"
+
+
+#use indexing and slicing to fill in the following output
+#copy and paste is not allowed
+
+file_name = url[-14:]
+print(file_name) // 'ted-sample.csv'
+
+
+
+
+protocol = url[0:5]
+print (protocol) #https
+
+host_name = url[8:18]
+print(host_name) #'github.com'
+
+#use string composition to construct http://github.com/ted-sample.csv
+output = protocol + "://" + host_name + '/' + file_name
